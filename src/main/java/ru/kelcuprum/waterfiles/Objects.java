b439bd60522dd57f98ca8256a882e8972cda1ea1
ImGRUI <91282421+ImGRUI@users.noreply.github.com>
2025-08-22 15:39:07 +0500

    more cleanup

--- src/main/java/ru/kelcuprum/waterfiles/Objects.java
+++ src/main/java/ru/kelcuprum/waterfiles/Objects.java
@@ -4,10 +4,8 @@ import com.google.gson.JsonObject;
 import ru.kelcuprum.caffeinelib.utils.GsonHelper;
 
 public class Objects {
-    public static JsonObject INDEX = GsonHelper.parseObject("{\"message\":\"Hello, world!\",\"url\":\"https://waterplayer.ru\"}");
     public static JsonObject NOT_FOUND = GsonHelper.parseObject("{\"error\":{\"code\":404,\"codename\":\"Not found\",\"message\":\"Method not found\"}}");
     public static JsonObject INTERNAL_SERVER_ERROR = GsonHelper.parseObject("{\"error\":{\"code\":500,\"codename\":\"Internal Server Error\",\"message\":\"\"}}");
-    public static JsonObject UNAUTHORIZED = GsonHelper.parseObject("{\"error\": {\"code\": 401,\"codename\": \"Unauthorized\",\"message\": \"You not authorized\"}}");
     public static JsonObject BAD_REQUEST = GsonHelper.parseObject("{\"error\": {\"code\": 400,\"codename\": \"Bad Request\",\"message\": \"The required arguments are missing!\"}}");
     public static JsonObject getErrorObject(Exception ex){
         JsonObject object = INTERNAL_SERVER_ERROR;
