22b22001eabcc54e141a7d770a8d527bf7b5a202
ImGRUI <91282421+ImGRUI@users.noreply.github.com>
2025-08-24 00:10:53 +0500

    100MB

--- src/main/java/ru/kelcuprum/waterfiles/Objects.java
+++ src/main/java/ru/kelcuprum/waterfiles/Objects.java
@@ -8,7 +8,7 @@ public class Objects {
     public static JsonObject INTERNAL_SERVER_ERROR = GsonHelper.parseObject("{\"error\":{\"code\":500,\"codename\":\"Internal Server Error\",\"message\":\"\"}}");
     public static JsonObject BAD_REQUEST = GsonHelper.parseObject("{\"error\": {\"code\": 400,\"codename\": \"Bad Request\",\"message\": \"Your request is bad.\"}}");
     public static JsonObject COUNT = GsonHelper.parseObject("{\"error\": {\"code\": 400,\"codename\": \"Bad Request\",\"message\": \"2 or more files are not supported!\"}}");
-    public static JsonObject PAYLOAD = GsonHelper.parseObject("{\"error\": {\"code\": 413,\"codename\": \"Payload Too Large\",\"message\": \"File is over 250mb!\"}}");
+    public static JsonObject PAYLOAD = GsonHelper.parseObject("{\"error\": {\"code\": 413,\"codename\": \"Payload Too Large\",\"message\": \"File is over 100mb!\"}}");
     public static JsonObject getErrorObject(Exception ex){
         JsonObject object = INTERNAL_SERVER_ERROR;
         object.get("error").getAsJsonObject().addProperty("message", ex.getMessage() == null ? ex.getClass().toString() : ex.getMessage());
--- src/main/java/ru/kelcuprum/waterfiles/Uploader.java
+++ src/main/java/ru/kelcuprum/waterfiles/Uploader.java
@@ -40,8 +40,8 @@ public class Uploader {
     public static final SecureRandom SECURE_RANDOM = new SecureRandom();
     public static final Tika tika = new Tika();
     public static final int Threshold = 1024 * 1024 * 5;
-    public static final int MaxFileSize = 1024 * 1024 * 250;
-    public static final int MaxRequestSize = 1024 * 1024 * 260;
+    public static final int MaxFileSize = 1024 * 1024 * 100;
+    public static final int MaxRequestSize = 1024 * 1024 * 100;
 
     public static void main(String[] args) throws IOException {
         try (InputStream releaseFile = Uploader.class.getResourceAsStream("/index.html")) {
